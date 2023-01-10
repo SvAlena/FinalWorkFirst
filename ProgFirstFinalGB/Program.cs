ae356670a3854650288446b6d1ccbfd032009422
«Alena» <a9214094331@gmail.com>
2023-01-10 16:36:52 +0300

    saving work

---
+++ ProgFirstFinalGB/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Enter an array of three lines: ");
+
+string[] array = new string[3];
+CreateArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+SortArray(array);
+
+void CreateArray(string[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine("The entered array: ");
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+void SortArray(string[] array)
+{
+    Console.WriteLine("A new array, where 3 characters or less: ");
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(array[i] + ", "); 
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
